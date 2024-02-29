1fb6f619d1ea33025a6a02cea80b85d90b90dd17
Manipalraaga <raagaraj922@gmail.com>
2024-02-29 23:46:59 +0530

    first commit

---
+++ src/test/java/ManipalAxis/OrangeHRM/LoginPortal.java
@@ -0,0 +1,46 @@
+package ManipalAxis.OrangeHRM;
+
+import org.testng.annotations.Test;
+
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.Test;
+
+	public class LoginPortal {
+	  @Test
+	  public void LoginPortalmethod() throws InterruptedException {
+		  
+		  System.setProperty("webdriver.chrome.driver",
+				  "C:\\Users\\amaan\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		  WebDriver driver = new ChromeDriver();
+			
+		  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers");
+			
+		  driver.manage().window().maximize();
+		  
+		  Thread.sleep(500);
+		  
+		  WebElement usernameinput = driver.findElement(By.xpath("//input[@name = 'username']"));
+		  
+	      WebElement passwordinput = driver.findElement(By.xpath("//input[@name = 'password']"));
+	      
+	      WebElement loginbutton = driver.findElement(By.xpath("//button[@type = 'submit']"));
+	      
+	      usernameinput.sendKeys("Admin");
+	      
+	      Thread.sleep(500);
+		  
+	      passwordinput.sendKeys("admin123");
+	      
+	      Thread.sleep(500);
+		  
+	      loginbutton.click();
+	      
+	      Thread.sleep(500);
+	      
+	      
+	  }
+	}
+	
